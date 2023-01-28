dd2f81592f139963ecf1b33babcdb813f1086d6a
pranavsk313 <122976840+pranavsk313@users.noreply.github.com>
2023-01-28 14:45:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World this is pranav kulkarni');
+  res.end('Hello World This is Pranav kulkarni');
 });
 server.listen(8080, function() {
 console.log('Server running on port 8080');
